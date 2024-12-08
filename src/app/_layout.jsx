2d9980f8851772a59772201d5c585389cef1defb
Atharva <teteatharva@gmail.com>
2024-12-08 11:33:36 +0530

    Sat 8 Dec 11:31am

--- src/app/_layout.jsx
+++ src/app/_layout.jsx
@@ -6,7 +6,7 @@ import * as SplashScreen from "expo-splash-screen";
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const [isLogin, setisLogin] = useState(true);
+  const [isLogin, setisLogin] = useState(false);
   useEffect(() => {
     setTimeout(() => {
       SplashScreen.hideAsync();
